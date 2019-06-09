86c0bede272adb960f6fefcc89577dd47628f8ac
solozyx <zhaoyingxin@aliyun.com>
2019-06-09 20:13:18 +0800

    consul 集群部署

---
+++ setup/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# 部署1节点server集群
+consul agent -server -bootstrap-expect 1 -data-dir /opt/soft/consul_data -node=n1 -bind=192.168.174.134 -ui -config-dir /opt/soft/consul_config -rejoin -join 192.168.174.134 -client 0.0.0.0 &
+
+# 防火墙放开 8301 和 8500 端口
+firewall-cmd --zone=public --add-port=8301/tcp --permanent
+firewall-cmd --zone=public --add-port=8500/tcp --permanent
+systemctl restart firewalld.service
+
+# 部署1节点client
+consul agent -data-dir /opt/soft/consul_data -node=n2 -bind=192.168.174.135 -config-dir /opt/soft/consul_config -rejoin -join 192.168.174.134 &
+
+# 查看集群状态
+consul members
+http://192.168.174.134:8500/ui/dc1/nodes

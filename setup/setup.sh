61d6460c48daef003191a8b51d0b5217fdf82f45
solozyx <zhaoyingxin@aliyun.com>
2019-06-10 11:45:41 +0800

    consul 服务注册发现

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,6 +11,12 @@ systemctl restart firewalld.service
 # 部署1节点client
 consul agent -data-dir /opt/soft/consul_data -node=n2 -bind=192.168.174.135 -config-dir /opt/soft/consul_config -rejoin -join 192.168.174.134 &
 
+firewall-cmd --zone=public --add-port=8301/tcp --permanent
+firewall-cmd --zone=public --add-port=8500/tcp --permanent
+systemctl restart firewalld.service
+
 # 查看集群状态
 consul members
 http://192.168.174.134:8500/ui/dc1/nodes
+dig @127.0.0.1 -p 8600 web.service.consul
+dig @127.0.0.1 -p 8600 web.service.consul SRV
